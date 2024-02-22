d2f25d539ce369974c7e2abaf09262ba1af32185
joliang17 <47235014+joliang17@users.noreply.github.com>
2024-02-22 14:05:26 -0500

    expr scripts

--- expr_shell/run_v4.sh
+++ expr_shell/run_v4.sh
@@ -3,12 +3,14 @@
 #SBATCH --job-name=flyp_loss_v4
 #SBATCH --output=flyp_loss_v4.out.%j
 #SBATCH --error=flyp_loss_v4.out.%j
-#SBATCH --time=24:00:00
-#SBATCH --account=scavenger 
-#SBATCH --partition=scavenger
-#SBATCH --gres=gpu:rtxa6000:1
-#SBATCH --cpus-per-task=4
+#SBATCH --time=18:00:00
+#SBATCH --account=cml-zhou
+#SBATCH --partition=cml-dpart
+#SBATCH --qos=cml-medium
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=6
 #SBATCH --mem=64G
+
 
 
 
@@ -28,4 +30,4 @@ SAVED_FOLDER="../data/metadata/clip_progress_train/"
 # python datacreation_scripts/iwildcam.py --mode="test" --save_folder=${SAVED_FOLDER} --input_folder=${TEST_FOLDER} --curriculum
 
 
-python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=200 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="./datasets/data/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}train.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v4" --curriculum --curriculum_epoch=10 --progress_eval --progress_train --scheduler=default --workers=4 --progress_metric="Acc" --progress --explore
+python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=256 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="./datasets/data/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}train.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v4" --curriculum --curriculum_epoch=10 --progress_eval --progress_train --scheduler=default --workers=4 --progress_metric="Acc" --progress --explore --slurm_job_id=$SLURM_JOB_ID
