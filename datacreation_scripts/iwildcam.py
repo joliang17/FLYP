e93eb0177bcc8df2bed009030f991be9cdf6c311
Joanna Liang <47235014+joliang17@users.noreply.github.com>
2024-03-08 20:50:55 -0500

    reformat path

--- datacreation_scripts/iwildcam.py
+++ datacreation_scripts/iwildcam.py
@@ -51,10 +51,11 @@ def main(args):
     iwildcam_template = [lambda c: f"a photo of {c}.", lambda c: f"{c} in the wild."]
     if args.mode in ('train', 'curriculum'):
         # for training and curriculum progress evaluation
-        label_to_name = pd.read_csv(args.label_file_ori)
+        label_to_name = pd.read_csv(f'{args.data_folder}/datasets/iwildcam_metadata/labels.csv')
+
     else:
         # test data
-        label_to_name = pd.read_csv(args.label_file_ori.replace('labels.csv', 'labels_new.csv'))
+        label_to_name = pd.read_csv(f'{args.data_folder}/datasets/iwildcam_metadata/labels_new.csv')
 
     label_to_name = label_to_name[label_to_name['y'] < 99999]
     label_to_name['prompt1'] = label_to_name['english'].map(iwildcam_template[0])
@@ -69,12 +70,12 @@ def main(args):
     list_y = []
     if args.mode == 'train':
         if args.curriculum:
-            clip_path = '../data/metadata/clip_score_train.pkl'
+            clip_path = f'{args.data_folder}/data/metadata/clip_score_train.pkl'
             threshold = 0.25
             Dict_filt = filter_img(clip_path, threshold)
 
             if args.topk_hard:
-                topk_path = '../data/metadata/clip_progress_train_img/generate.csv'
+                topk_path = f'{args.data_folder}/data/metadata/clip_progress_train_img/generate.csv'
                 Dict_filt = filter_img_topk(topk_path, Dict_filt)
 
             all_cnt = 0
@@ -109,7 +110,7 @@ def main(args):
         if not args.total_train:
             #############################################
             # if using part of the original training data
-            data_path = "../data/train"
+            data_path = f"{args.data_folder}/data/train"
             img_sp_folder_ori = os.listdir(data_path)
             img_sp_folder_ori = [item for item in img_sp_folder_ori if item in img_sp_folder]
             cur_strength = 0
@@ -128,7 +129,7 @@ def main(args):
         else:
             #############################################
             # if using all training data
-            df_train_ori = pd.read_csv('./datasets/csv/iwildcam_v2.0/train.csv', sep='\t')
+            df_train_ori = pd.read_csv(f'{args.data_folder}/datasets/csv/iwildcam_v2.0/train.csv', sep='\t')
             del df_train_ori['title']
             df_train_ori.drop_duplicates(subset=['filepath', 'label'], keep='last', inplace=True)
             df_train_ori.rename({'filepath': 'filename', 'label': 'y'}, axis='columns', inplace=True)
@@ -140,11 +141,11 @@ def main(args):
     elif args.mode == 'curriculum':
         # for curriculum progress evaluation
 
-        clip_path = '../data/metadata/clip_score_curriculum.pkl'
+        clip_path = f'{args.data_folder}/data/metadata/clip_score_curriculum.pkl'
         threshold = 0.25
         Dict_filt = filter_img(clip_path, threshold)
 
-        data_path = "../data/test_new"
+        data_path = f"{args.data_folder}/data/test_new"
         img_sp_folder_curri = os.listdir(data_path)
         img_sp_folder_curri = [item for item in img_sp_folder_curri if item.replace('_', ' ') in sp_name]
 
@@ -185,22 +186,26 @@ def main(args):
                         list_result.append([cur_y, new_path, cur_strength])
 
     else:
-        img_sp_folder_train = os.listdir("../data/train_new")
-        img_sp_folder_train = [item for item in img_sp_folder if item.replace('_', ' ') in sp_name]
+        raise ValueError(f"No test supported!")
+        """
+        # # test eval
+        # img_sp_folder_train = os.listdir("../data/train_new")
+        # img_sp_folder_train = [item for item in img_sp_folder if item.replace('_', ' ') in sp_name]
 
-        img_sp_folder_test = os.listdir(args.input_folder)
-        img_sp_folder_test = [item for item in img_sp_folder_test if item in img_sp_folder_train]
+        # img_sp_folder_test = os.listdir(args.input_folder)
+        # img_sp_folder_test = [item for item in img_sp_folder_test if item in img_sp_folder_train]
 
-        cur_strength = 0
-        for cur_sp_f in img_sp_folder_test:
-            cur_sp_path = os.path.join(args.input_folder, cur_sp_f)
-            cur_sp_name = cur_sp_f.replace('_', ' ')
-            cur_y = label_to_name[label_to_name['name'] == cur_sp_name]['y'].values[0]
-            list_imgs = os.listdir(cur_sp_path)
-            list_imgs = [item for item in list_imgs if 'jpg' in item]
-            for img_name in list_imgs:
-                cur_img_path = os.path.join(cur_sp_path, img_name)
-                list_result.append([cur_y, cur_img_path, cur_strength])
+        # cur_strength = 0
+        # for cur_sp_f in img_sp_folder_test:
+        #     cur_sp_path = os.path.join(args.input_folder, cur_sp_f)
+        #     cur_sp_name = cur_sp_f.replace('_', ' ')
+        #     cur_y = label_to_name[label_to_name['name'] == cur_sp_name]['y'].values[0]
+        #     list_imgs = os.listdir(cur_sp_path)
+        #     list_imgs = [item for item in list_imgs if 'jpg' in item]
+        #     for img_name in list_imgs:
+        #         cur_img_path = os.path.join(cur_sp_path, img_name)
+        #         list_result.append([cur_y, cur_img_path, cur_strength])
+        """
 
     df = pd.DataFrame(list_result, columns=['y', 'filename', 'strength'])
     df.loc[:, 'guidance'] = df['strength'].apply(lambda x: 100 - int(x))
@@ -239,19 +244,6 @@ def main(args):
         df = df_list
         print(len(df))
 
-    #####################################
-    # only train categories with enhanced data
-    # if args.mode == 'train':
-    #     list_y = sorted(list_y, reverse=False)
-    #     list_label_csv = label_to_name['y'].unique().tolist()
-    #     list_missing = list(set(list_label_csv) - set(list_y))
-    #     list_names = [label_to_name[label_to_name['y']==i]['name'].values[0] for i in list_missing]
-    #     pdb.set_trace()
-    #     label_to_name = label_to_name[label_to_name['y'].isin(list_y)]
-    #     # pdb.set_trace()
-    #     new_path = args.label_file_ori.replace('labels.csv', 'labels_new.csv')
-    #     label_to_name.to_csv(new_path, index=False)
-
     # merge prompts
     df1 = pd.merge(df, label_to_name[['y', 'prompt1']], on='y').rename({'prompt1': 'title'}, axis='columns')
     df2 = pd.merge(df, label_to_name[['y', 'prompt2']], on='y').rename({'prompt2': 'title'}, axis='columns')
@@ -277,7 +269,7 @@ if __name__ == '__main__':
     parser.add_argument('--total_train', action=argparse.BooleanOptionalAction)
     parser.add_argument('--save_folder', default='./datasets/csv/iwildcam_v2.0/')
     parser.add_argument('--input_folder', default='../data/train_new')
-    parser.add_argument('--label_file_ori', default='./src/datasets/iwildcam_metadata/labels.csv')
+    parser.add_argument('--data_folder', default='/fs/nexus-scratch/yliang17/Research/diffusion/gene_diffcls')
     args = parser.parse_args()
 
     os.makedirs(args.save_folder, exist_ok=True)
