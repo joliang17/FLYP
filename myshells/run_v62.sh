a72655f4970ef756729d97fc030f591a13b736cb
joliang17 <47235014+joliang17@users.noreply.github.com>
2024-03-16 20:15:19 -0400

    add expr shells

--- myshells/run_v62.sh
+++ myshells/run_v62.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=flyp_loss_v62
-#SBATCH --output=flyp_loss_v62.out.%j
-#SBATCH --error=flyp_loss_v62.out.%j
+#SBATCH --job-name=v62_3
+#SBATCH --output=v62_3.out.%j
+#SBATCH --error=v62_3.out.%j
 #SBATCH --time=48:00:00
-#SBATCH --account=cml-zhou
-#SBATCH --partition=cml-dpart
-#SBATCH --qos=cml-medium
-#SBATCH --gres=gpu:a100:1
-#SBATCH --cpus-per-task=6
+#SBATCH --account=scavenger 
+#SBATCH --partition=scavenger
+#SBATCH --gres=gpu:rtxa6000:1
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 
 
@@ -29,5 +28,5 @@ SAVED_FOLDER="../data/metadata/clip_progress_train/"
 # python datacreation_scripts/iwildcam.py --mode="test" --save_folder=${SAVED_FOLDER} --input_folder=${TEST_FOLDER} --curriculum
 
 
-python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=300 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="../data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}train.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v62" --curriculum --curriculum_epoch=10 --progress_eval --progress_train --scheduler=default --workers=4 --progress_metric="Acc" --progress --explore --slurm_job_id=$SLURM_JOB_ID --cont_finetune --uniform_set
+python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=200 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="../data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}train.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v62_3" --curriculum --curriculum_epoch=10 --progress_eval --progress_train --scheduler=default --workers=4 --progress_metric="Acc" --progress --explore --slurm_job_id=$SLURM_JOB_ID --uniform_set
 
---
+++ myshells/run_v700_2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --job-name=v700_2
+#SBATCH --output=v700_2.out.%j
+#SBATCH --error=v700_2.out.%j
+#SBATCH --time=48:00:00
+#SBATCH --account=cml-zhou
+#SBATCH --partition=cml-dpart
+#SBATCH --qos=cml-medium
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=64G
+
+
+
+
+# checking gpu status
+nvidia-smi
+
+# cd ../..
+source /fs/nexus-scratch/yliang17/miniconda3/bin/activate diffu
+
+
+TRAIN_FOLDER="../data/train_new"
+TEST_FOLDER="../data/test"
+SAVED_FOLDER="../data/metadata/clip_progress_difficult_2022/"
+
+# python datacreation_scripts/iwildcam.py --mode="train" --save_folder=${SAVED_FOLDER} --input_folder=${TRAIN_FOLDER} --curriculum --total_train
+
+# # curriculum tau version
+python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=256 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="../data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}train.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v700_2" --baseline --scheduler=default --workers=4 --slurm_job_id=$SLURM_JOB_ID
+
