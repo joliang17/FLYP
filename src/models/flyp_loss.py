3d6027a8d0bf1c224ec2bc9c726258c380226d7e
joliang17 <47235014+joliang17@users.noreply.github.com>
2024-03-19 12:14:31 -0400

    fixed bug

--- src/models/flyp_loss.py
+++ src/models/flyp_loss.py
@@ -76,7 +76,7 @@ def load_data(logger, args, clip_encoder, cur_guidance=None, cur_str_times=1, li
     # load dataloader
     img_text_data = get_data(args, (clip_encoder.train_preprocess, clip_encoder.val_preprocess), epoch=0,
                              guidance=cur_guidance, list_selection=list_classes, ori_proportion=ori_proportion,
-                             uniform_set=uniform_set, return_img_id=return_img_id, include_neg=include_neg
+                             uniform_set=uniform_set, return_img_id=return_img_id, include_neg=include_neg,
                              reshift_distribution=reshift_distribution, logger=logger)
     assert len(img_text_data), 'At least one train or eval dataset must be specified.'
 
