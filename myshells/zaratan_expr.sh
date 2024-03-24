93ff18865ba14af1b587190c20a74ffcf101a9b4
Yijun Liang <47235014+joliang17@users.noreply.github.com>
2024-03-24 00:30:55 -0400

    modify zaratan path

--- myshells/zaratan_expr.sh
+++ myshells/zaratan_expr.sh
@@ -10,39 +10,55 @@
 
 . ~/.bashrc
 export SLURM_EXPORT_ENV=ALL
-# module purge
-# module load python
 
 # Section to output information identifying the job, etc.
 echo "Slurm job ${SLURM_JOBID} running on ${hostname}"
 echo "To run on ${SLURM_NTASKS} CPU cores across ${SLURM_JOB_NUM_NODES} nodes"
 echo "All nodes: ${SLURM_JOB_NODELIST} ${date} ${pwd}"
-# echo "Loaded modules are:"
-# module list
-
-# checking gpu status
-# nvidia-smi
 
 current_dir=$(pwd)
 if [[ "$current_dir" == *"zt1/project/tianyi-prj"* ]]; then
     # on zaratan cluster
 
     conda activate flyp
-    root_folder="/scratch/zt1/project/tianyi-prj/user/yliang17/Research"
-    saved_ckpt="."
-    saved_data="${root_folder}/gene_diffcls"
+    scratch_root="/home/yliang17/scratch.tianyi-prj/Research/"
+
+    # location of original imgs
+    root_folder="${scratch_root}/gene_diffcls"
+    # location of cache files
+    cache_folder="${scratch_root}/cache"
+
+    # location of generated images
+    shell_root="/home/yliang17/SHELL.tianyi-prj/Research/"
+    shell_folder="${shell_root}/gene_diffcls"
+    IMG_FOLDER="${shell_folder}/data/train_new"
 
     echo "On zaratan node"
 else
     # on uniacs cluster
 
     source /fs/nexus-scratch/yliang17/miniconda3/bin/activate diffu
-    root_folder="/fs/nexus-scratch/yliang17/Research"
-    saved_ckpt="."
-    saved_data="${root_folder}/diffusion/gene_diffcls"
+    scratch_root="/fs/nexus-scratch/yliang17/Research"
+
+    # location of original imgs
+    root_folder=".."
+    # location of cache files
+    cache_folder="${scratch_root}/cache"
+
+    # location of generated images
+    IMG_FOLDER="${root_folder}/data/train_new"
 fi
 
-python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=128 --model=ViT-L-14 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save="${saved_ckpt}/checkpoints/" --data-location="${saved_data}/data/iwildcam/" --ft_data="${saved_data}/data/iwildcam/iwildcam_v2.0/train.csv" --csv-img-key filepath --csv-caption-key title --exp_name=flyp_loss_base_vitl --workers=4 --baseline  --slurm_job_id=$SLURM_JOB_ID --cache_folder="${root_folder}/cache"
+
+# location of generated train.csv / curriculum.csv / used pkl ..
+META_FOLDER="${root_folder}/data/metadata"
+SAVED_FOLDER="${META_FOLDER}/clip_progress_difficult_2022_5_onlyguid/"
+
+python datacreation_scripts/iwildcam.py --save_folder="${SAVED_FOLDER}" --input_folder="${IMG_FOLDER}" --curriculum --gene_constr="${META_FOLDER}/used_imgid/used_imgid_v5.pkl"
+
+# uniform dataset + guid >= 50
+python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=300 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save="./checkpoints/" --data-location="${root_folder}/data/iwildcam/" --ft_data="${SAVED_FOLDER}/train.csv" --ft_data_test="${SAVED_FOLDER}/curriculum.csv" --cache_folder="${cache_folder}" --csv-img-key filepath --csv-caption-key title --workers=4 --exp_name="flyp_loss_v715_rerun" --curriculum --curriculum_epoch=10 --progress_guid --uniform_set --progress_metric=Prob --scheduler=default --slurm_job_id=$SLURM_JOB_ID
+
 
 ECODE=$?
 	
