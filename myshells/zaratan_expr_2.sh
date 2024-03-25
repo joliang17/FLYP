12dba4b84dee9f430d0d572ef413e113732c4732
Yijun Liang <47235014+joliang17@users.noreply.github.com>
2024-03-25 11:19:40 -0400

    fixed no guidance bugs

--- myshells/zaratan_expr_2.sh
+++ myshells/zaratan_expr_2.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=flyp_loss_base_vitl
 #SBATCH --account=tianyi-prj-cmsc
 #SBATCH --time=24:00:00
-#SBATCH --gpus=a100:1
+#SBATCH --gpus=a100:2
 #SBATCH --partition=gpu
 #SBATCH --mem-per-cpu=128
 #SBATCH --ntasks=6
@@ -58,7 +58,7 @@ SAVED_FOLDER="${META_FOLDER}/clip_progress_difficult_2022_5_onlyguid_zrt/"
 #python datacreation_scripts/iwildcam.py --save_folder="${SAVED_FOLDER}" --input_folder="${IMG_FOLDER}" --curriculum --gene_constr="${META_FOLDER}/used_imgid/used_imgid_v5.pkl"
 
 # uniform dataset + guid >= 50
-python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=300 --model=ViT-L-14 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save="./checkpoints/" --data-location="../data/iwildcam/" --ft_data="../data/iwildcam/iwildcam_v2.0/train.csv" --cache_folder="${cache_folder}" --csv-img-key filepath --csv-caption-key title --workers=4 --exp_name="vit_L" --baseline --slurm_job_id=$SLURM_JOB_ID --debug
+python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=128 --model=ViT-L-14 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save="./checkpoints/" --data-location="../data/iwildcam/" --ft_data="../data/iwildcam/iwildcam_v2.0/train.csv" --cache_folder="${cache_folder}" --csv-img-key filepath --csv-caption-key title --workers=4 --exp_name="vit_L" --baseline --slurm_job_id=$SLURM_JOB_ID --debug
 
 
 ECODE=$?
