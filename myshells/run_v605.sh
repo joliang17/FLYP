6af61d74d507753187daae01b84374425feeade0
joliang17 <47235014+joliang17@users.noreply.github.com>
2024-03-29 15:44:11 -0400

    add shell

--- myshells/run_v605.sh
+++ myshells/run_v605.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=flyp_loss_v605
-#SBATCH --output=flyp_loss_v605.out.%j
-#SBATCH --error=flyp_loss_v605.out.%j
-#SBATCH --time=48:00:00
-#SBATCH --account=cml-zhou
-#SBATCH --partition=cml-dpart
-#SBATCH --qos=cml-medium
-#SBATCH --gres=gpu:a100:1
-#SBATCH --cpus-per-task=6
+#SBATCH --job-name=v605
+#SBATCH --output=v605.out.%j
+#SBATCH --error=v605.out.%j
+#SBATCH --time=24:00:00
+#SBATCH --account=scavenger 
+#SBATCH --partition=scavenger
+#SBATCH --gres=gpu:rtxa6000:1
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
-
-
 
 
 # checking gpu status
@@ -23,9 +20,9 @@ source /fs/nexus-scratch/yliang17/miniconda3/bin/activate diffu
 
 TRAIN_FOLDER="../data/train_new"
 TEST_FOLDER="../data/test"
-SAVED_FOLDER="../data/metadata/clip_progress_difficult_2022/"
+SAVED_FOLDER="../data/metadata/clip_progress_difficult_2022_51/"
 
-#  python datacreation_scripts/iwildcam.py --mode="train" --save_folder=${SAVED_FOLDER} --input_folder=${TRAIN_FOLDER} --curriculum --total_train --gene_constr='../data/metadata/used_imgid/used_imgid_v3.pkl'
+#  python datacreation_scripts/iwildcam.py --mode="train" --save_folder=${SAVED_FOLDER} --input_folder=${TRAIN_FOLDER} --curriculum --total_train --gene_constr='../data/metadata/used_imgid/used_imgid_v5.pkl'
 
-python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=300 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="../data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}train.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v605" --curriculum --curriculum_epoch=10 --progress_eval --progress_train --scheduler=default --workers=4 --progress_metric="Acc" --progress --explore --slurm_job_id=$SLURM_JOB_ID --cont_finetune
+python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=200 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="../data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}train.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v605" --curriculum --curriculum_epoch=10 --progress_eval --progress_train --scheduler=default --workers=4 --progress_metric="Acc" --progress --explore --slurm_job_id=$SLURM_JOB_ID
 
--- myshells/run_v605_half.sh
+++ myshells/run_v605_half.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=v605_half
 #SBATCH --output=v605_half.out.%j
 #SBATCH --error=v605_half.out.%j
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --account=cml-zhou
 #SBATCH --partition=cml-dpart
 #SBATCH --qos=cml-medium
@@ -31,5 +31,5 @@ SAVED_FOLDER="../data/metadata/clip_progress_difficult_2022/"
 
 
 # uniformly distribution + no randomness + train from pretrain
-python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=300 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="../data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}train.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v605_half" --curriculum --curriculum_epoch=10 --progress_eval --progress_train --scheduler=default --workers=4 --progress_metric="Acc" --progress --explore --slurm_job_id=$SLURM_JOB_ID
+python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=300 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="../data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}train.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v605_half_2" --curriculum --curriculum_epoch=10 --progress_eval --progress_train --scheduler=default --workers=4 --progress_metric="Acc" --progress --explore --slurm_job_id=$SLURM_JOB_ID
 
