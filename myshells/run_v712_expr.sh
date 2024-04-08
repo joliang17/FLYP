a4011e58795b8ad58d09c0327117f3af6b81406a
joliang17 <47235014+joliang17@users.noreply.github.com>
2024-04-08 17:33:48 -0400

    exprs

--- myshells/run_v712_expr.sh
+++ myshells/run_v712_expr.sh
@@ -22,10 +22,9 @@ source /fs/nexus-scratch/yliang17/miniconda3/bin/activate diffu
 
 TRAIN_FOLDER="../data/train_new"
 TEST_FOLDER="../data/test"
-SAVED_FOLDER="../data/metadata/clip_progress_difficult_2022_2_onlyguid/"
+SAVED_FOLDER="../data/metadata/clip_progress_difficult_2022_randseed_expr/"
 
 # python datacreation_scripts/iwildcam.py --save_folder=${SAVED_FOLDER} --input_folder=${TRAIN_FOLDER} --curriculum --gene_constr='../data/metadata/used_imgid/used_imgid_v2.pkl'
 
-# uniform dataset + guid >= 50
-python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=200 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="../data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}curriculum.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v712_expr" --curriculum --curriculum_epoch=15 --progress_guid --uniform_set --progress_metric=Prob --scheduler=default --workers=4 --slurm_job_id=$SLURM_JOB_ID --debug
+python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=200 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="../data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}curriculum.csv" --csv-img-key filepath --csv-caption-key title --exp_name="progress_expr" --curriculum --curriculum_epoch=15 --progress_guid --uniform_set --progress_metric=Prob --proportion --scheduler=default --workers=4 --slurm_job_id=$SLURM_JOB_ID --debug
 
