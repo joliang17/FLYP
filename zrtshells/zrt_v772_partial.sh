48428241d9018ad9c50129d81786b6a5b3a108fd
Yijun Liang <47235014+joliang17@users.noreply.github.com>
2024-04-25 10:10:29 -0400

    change running time

--- zrtshells/zrt_v772_partial.sh
+++ zrtshells/zrt_v772_partial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=v772_p
 #SBATCH --account=tianyi-prj-cmsc
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --gpus=a100:1
 #SBATCH --partition=gpu
 #SBATCH --mem-per-cpu=10000
