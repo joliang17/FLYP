62cde2f1471e9108f3eebf5fb393d1409b55162c
joliang17 <47235014+joliang17@users.noreply.github.com>
2024-05-06 22:24:17 -0400

    saved shells

---
+++ myshells/run_v81_nocurr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=v81
+#SBATCH --output=v81.out.%j
+#SBATCH --error=v81.out.%j
+#SBATCH --time=48:00:00
+#SBATCH --account=scavenger 
+#SBATCH --partition=scavenger
+#SBATCH --gres=gpu:rtxa6000:2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+
+
+
+# checking gpu status
+nvidia-smi
+
+# cd ../..
+source /fs/nexus-scratch/yliang17/miniconda3/bin/activate diffu
+
+
+TRAIN_FOLDER="../data/train_new"
+TEST_FOLDER="../data/test"
+SAVED_FOLDER="../data/metadata/clip_progress_difficult_2022_2_onlyguid_expr/"
+
+# while train with guid != 100, merge it with all other guid = 100 data
+python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=300 --model=ViT-B/16 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save=./checkpoints/ --data-location="../data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}curriculum.csv" --csv-img-key filepath --csv-caption-key title --exp_name="flyp_loss_v81" --scheduler=default --workers=4 --slurm_job_id=$SLURM_JOB_ID --wandb_tag="ablation"
+
