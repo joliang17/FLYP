5c535368817254f4fe2ff26c2bee3e572c7672f7
Yijun Liang <47235014+joliang17@users.noreply.github.com>
2024-05-14 16:21:10 -0400

    number batch

--- zrtshells/zrt_v752_vitl.sh
+++ zrtshells/zrt_v752_vitl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=v752
 #SBATCH --account=tianyi-prj-cmsc
-#SBATCH --time=50:00:00
+#SBATCH --time=70:00:00
 #SBATCH --gpus=h100:1
 #SBATCH --partition=gpu
 #SBATCH --mem-per-cpu=10000
@@ -43,12 +43,12 @@ root_folder=".."
 # location of generated train.csv / curriculum.csv / used pkl ..
 IMG_FOLDER="${root_folder}/data/train_new"
 META_FOLDER="${root_folder}/data/metadata"
-SAVED_FOLDER="${META_FOLDER}/clip_progress_difficult_2022_5_onlyguid/"
+SAVED_FOLDER="${META_FOLDER}/difficult_2_sample/"
 
-python datacreation_scripts/iwildcam.py --save_folder="${SAVED_FOLDER}" --input_folder="${IMG_FOLDER}" --curriculum --gene_constr="${META_FOLDER}/used_imgid/used_imgid_v5.pkl" --data_folder="${root_folder}"
+#python datacreation_scripts/iwildcam.py --save_folder="${SAVED_FOLDER}" --input_folder="${IMG_FOLDER}" --curriculum --gene_constr="${META_FOLDER}/used_imgid/used_imgid_v5.pkl" --data_folder="${root_folder}"
 
 # uniform dataset + guid >= 50
-python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=100 --model=ViT-L/14 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save="./checkpoints/" --data-location="${root_folder}/data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}curriculum.csv"  --cache_folder="${cache_folder}" --csv-img-key filepath --csv-caption-key title --workers=4 --exp_name="v715"  --curriculum --curriculum_epoch=15 --progress_guid --uniform_set --merge_ori --progress_metric=Prob --scheduler=default --workers=4 --slurm_job_id=$SLURM_JOB_ID --debug
+python src/main.py --train-dataset=IWildCamIDVal --epochs=20 --lr=1e-5 --wd=0.2 --batch-size=100 --model=ViT-L/14 --eval-datasets=IWildCamIDVal,IWildCamID,IWildCamOOD --template=iwildcam_template  --save="./checkpoints/" --data-location="${root_folder}/data/iwildcam/" --ft_data="${SAVED_FOLDER}train.csv" --ft_data_test="${SAVED_FOLDER}curriculum.csv"  --cache_folder="${cache_folder}" --csv-img-key filepath --csv-caption-key title --workers=4 --exp_name="v752"  --curriculum --curriculum_epoch=15 --progress_guid --uniform_set --merge_ori --progress_metric=Prob --scheduler=default --workers=4 --slurm_job_id=$SLURM_JOB_ID --debug
 
 
 ECODE=$?
